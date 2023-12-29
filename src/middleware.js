08efa56f6a25f1562af6c876da4d1f584e23f03e
Rogelio Vargas Marquez <rogelio.vargas.marquez@gmail.com>
2023-12-29 04:05:52 -0600

    fix(route): issues unsolved with dynamioc params

--- src/middleware.js
+++ src/middleware.js
@@ -6,7 +6,17 @@ export default createMiddleware({
   defaultLocale: 'en',
   // A list of all locales that are supported
   locales,
-  localePrefix
+  localePrefix,
+  domains: [
+    {
+      domain: 'rogeliovargas.com',
+      defaultLocale: 'en',
+      // Optionally restrict the locales managed by this domain. If this
+      // domain receives requests for another locale (e.g. us.example.com/fr),
+      // then the middleware will redirect to a domain that supports it.
+      locales
+    },
+  ]
 });
  
 export const config = {
